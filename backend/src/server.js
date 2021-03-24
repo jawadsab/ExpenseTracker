582f605c3ccbbfd53bc8ff4105ad906e86320035
Mohammed Abdul Jawad Vajeeh <51875089+jawadsab@users.noreply.github.com>
2021-03-24 11:18:08 +0530

    mongoose setup, mongodb connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,25 @@ const express = require('express');
 const app = express();
 const colors = require('colors');
 const morgan = require('morgan');
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+
+    const conn = await mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser:true,
+      useCreateIndex:true,
+      useUnifiedTopology:true,
+    });
+
+    console.log(`MongoDB connected ${conn.connection.host}`.cyan.underline.bold);
+    
+  } catch (err) {
+    console.log(`Error ${err.message}`.red);
+    process.exit(1);
+  }
+}
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
